d841181e8fca3119a0de29317d65dd5c8e8e6e4f
phnguyen <phnguyen@student.42.fr>
2020-08-22 07:00:09 +0200

    test errno

--- ft_write.s
+++ ft_write.s
@@ -2,21 +2,21 @@
 ; Care of the call of errno -> ___error or ___errno_location
 
 segment .text
-    global  _ft_write
-    extern  ___error
+	global	_ft_write
+	extern	___error
 
 _ft_write:
 
-    mov     r8, rdx
-	mov     rax, 0x2000004
-    syscall
+	mov		r8, rdx
+	mov		rax, 0x2000004
+	syscall
 	jc		failure
-    jmp		exit
+	jmp		exit
 
 failure:
 	mov		r8, rax
-    call	___error
-    mov		[rax], r8
+	call	___error
+	mov		[rax], r8
 	mov		rax, -1
 	ret
 
