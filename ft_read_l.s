74704dcea34116a6862378e2827e0a07dfe5f571
phnguyen <phnguyen@student.42.fr>
2020-08-28 01:40:26 +0200

    fix behavior of read and write on linux

--- ft_read_l.s
+++ ft_read_l.s
@@ -6,13 +6,15 @@ segment .text
 ft_read:
 	mov     rax, 0
     syscall
-    jc		failure
+	cmp		rax, 0
+    jl		failure
 	ret
 
 failure:
 	push	rax
 	call	___errno_location
 	pop		r8
+	neg		r8
 	mov		[rax], r8
 	mov		rax, -1
 	ret
--- ft_write_l.s
+++ ft_write_l.s
@@ -7,13 +7,15 @@ ft_write:
 
 	mov		rax, 1
 	syscall
-	jc		failure
+	cmp		rax, 0
+	jl		failure
 	ret
 
 failure:
 	push	rax
 	call	___errno_location
 	pop		r8
+	neg		r8
 	mov		[rax], r8
 	mov		rax, -1
 	ret
