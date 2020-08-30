fe6197b8dccff85a10bff4f23f5153ce6c0a719f
slapshiii <phi-long93@hotmail.fr>
2020-08-30 23:30:15 +0200

    list sort test todo

--- ft_list_sort_bonus.s
+++ ft_list_sort_bonus.s
@@ -8,52 +8,45 @@ section	.text
 
 _ft_list_sort:
 	push	rsp
-	push	r14
-	push	r15
+	push	r14				;temp1
+	push	r15				;temp2
 
-	xor		rcx, rcx		;current
-	xor		rdx, rdx		;bool swap
+	mov		r8, [rdi]		;first (save)
+	mov		rdi, [rdi]		;first
 	xor		r9, r9			;next
 	cmp 	rdi, 0
 	jz		exit
 	cmp 	rsi, 0
 	jz		exit
-	mov		r8, [rdi]		;first
-	mov		r14, rsi
-	jmp		back_start
-
-back_start:
-	mov		rcx, r8
-	mov		r9, [rcx + 8]
+	
+	mov		r14, rsi		;save (cmp)
 	jmp		loop
 
 loop:
+	mov		r9, [rdi + 8]
 	cmp		r9, 0
-	jz		check_end
-	mov		rdi, [rcx]
+	jz		exit
+	push	rdi
+	push	rsi
 	mov		rsi, [r9]
+	mov		rdi, [rdi]
 	call	r14
+	pop		rsi
+	pop		rdi
 	cmp		rax, 0
 	jz		swap
 	jmp		go_next
 
 swap:						;swap rdi rsi
-	mov		r15, rdi
-	mov		rdi, rsi
-	mov		rsi, r15
-	mov		rdx, 1
+	mov		r14, [r9]
+	mov		r15, [rdi]
+	mov		[r9], r15
+	mov		[rdi], r14
 	jmp		go_next
 
 go_next:
-	mov		r15, r9
-	mov		r9, [r9 + 8]
-	mov		rcx, r15
+	mov		rdi, [rdi + 8]
 	jmp		loop
-
-check_end:
-	cmp		rdx, 1
-	je		back_start
-	jmp		exit
 
 exit:
 	pop		r15
